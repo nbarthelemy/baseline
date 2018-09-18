839fd5efc08c6bc4053a6df3117a38874032bc14
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-09-18 09:16:15 -0500

    Allow html flashes

--- apps/_gems/core/app/controllers/core/application_controller.rb
+++ apps/_gems/core/app/controllers/core/application_controller.rb
@@ -4,5 +4,15 @@ module Core
 
     protect_from_forgery prepend: true
 
+    before_action :permit_html_flashes, if: proc { flash[:html_safe] == true }
+
+  private
+
+    def permit_html_flashes
+      flash.keys.reject{|k| k == :html_safe }.each do |k|
+        flash.now[k] = flash[k].to_s.html_safe
+      end
+    end
+
   end
 end
